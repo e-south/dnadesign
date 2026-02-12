b8dc5d6611925b7749b7381634fa687842e8856d
Eric South <esouth@scc1.bu.edu>
2026-02-12 15:03:18 -0500

    Register notify send command via cli_commands module

--- src/dnadesign/notify/cli_commands/__init__.py
+++ src/dnadesign/notify/cli_commands/__init__.py
@@ -10,12 +10,14 @@ Module Author(s): Eric J. South
 """
 
 from .profile import register_profile_commands
+from .send import register_send_command
 from .setup import register_setup_commands
 from .spool import register_spool_drain_command
 from .usr_events import register_usr_events_watch_command
 
 __all__ = [
     "register_profile_commands",
+    "register_send_command",
     "register_setup_commands",
     "register_spool_drain_command",
     "register_usr_events_watch_command",
