082c13e13caf71949dab714677e8ed89c55e96fa
Eric South <e-south@users.noreply.github.com>
2026-08-01 10:29:44 -0400

    fix(baserender): restore canonical module headers

--- src/dnadesign/baserender/src/config/jobs/__init__.py
+++ src/dnadesign/baserender/src/config/jobs/__init__.py
@@ -1,4 +1,13 @@
-"""Versioned BaseRender job contracts."""
+"""
+--------------------------------------------------------------------------------
+dnadesign
+src/dnadesign/baserender/src/config/jobs/__init__.py
+
+Versioned BaseRender job contracts.
+
+Module Author(s): Eric J. South
+--------------------------------------------------------------------------------
+"""
 
 from .base_render_v4 import *  # noqa: F403
 from .base_render_v4 import __all__ as __all__
--- src/dnadesign/baserender/src/execution/__init__.py
+++ src/dnadesign/baserender/src/execution/__init__.py
@@ -1,4 +1,13 @@
-"""Render-job execution orchestration."""
+"""
+--------------------------------------------------------------------------------
+dnadesign
+src/dnadesign/baserender/src/execution/__init__.py
+
+Render-job execution orchestration.
+
+Module Author(s): Eric J. South
+--------------------------------------------------------------------------------
+"""
 
 from .runner import run_render_job
 
